158b1c490625551764fff059b052adbe5ed7642b
Minh Trần Quang <minhtq30102000@gmail.com>
2020-09-05 16:59:49 +0700

    add slider

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Slider;
 use Illuminate\Http\Request;
+use function Symfony\Component\String\s;
 
 class HomeController extends Controller
 {
+    private $slider;
+
+    public function __construct(Slider $slider){
+        $this->slider = $slider;
+    }
+
     public function index()
     {
-        return view('home.home');
+        $sliders = $this->slider->latest()->get();
+        return view('home.home', compact('sliders'));
     }
 
     public function test()
