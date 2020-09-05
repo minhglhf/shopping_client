71a56ee2fd0e216c97f2064e9b2121616e62cab1
Minh Trần Quang <minhtq30102000@gmail.com>
2020-09-05 11:35:37 +0700

    view Layouts, header, footer

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>@yield('title')</title>
+    <link href="{{asset('eshopper/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('eshopper/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('eshopper/css/prettyPhoto.css')}}" rel="stylesheet">
+    <link href="{{asset('eshopper/css/price-range.css')}}" rel="stylesheet">
+    <link href="{{asset('eshopper/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('eshopper/css/main.css')}}" rel="stylesheet">
+
+    @yield('css')
+</head>
+
+
+<body>
+{{--@section('sidebar')--}}
+{{--    This is the master sidebar.--}}
+{{--@show--}}
+
+@include('components.header')
+
+@yield('content')
+
+@include('components.footer')
+
+</body>
+
+<script src="{{asset('eshopper/js/jquery.js')}}"></script>
+<script src="{{asset('eshopper/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('eshopper/js/jquery.scrollUp.min.js')}}"></script>
+<script src="{{asset('eshopper/js/price-range.js')}}"></script>
+<script src="{{asset('eshopper/js/jquery.prettyPhoto.js')}}"></script>
+<script src="{{asset('eshopper/js/main.js')}}"></script>
+@yield('js')
+</html>
